7730173090b0a9c5289d6c4452d787c154282ed4
Pronav05 <pforpranav05@gmail.com>
2020-12-22 18:01:33 +0800

    plinko with scores

--- js/Particle.js
+++ js/Particle.js
@@ -8,12 +8,35 @@ class Particle {
       
         this.body = Bodies.circle(x, y, this.r,options);       
         this.color=color(random(0, 255), random(0, 255), random(0, 255));
+      
+        
         World.add(world, this.body);
 
     }
-    display() {
-
+    score(){
+        
         var pos = this.body.position;
+        if(pos.y > 700){
+            if(pos.x < 300 && score1 == false){
+                score += 500;
+                score1 = true;
+                console.log(score);
+                //console.log(score1);
+            }
+            if(pos.x > 300 && pos.x < 600){
+                score += 100;
+                console.log(score);
+            }
+            if(pos.x > 600){
+                score += 200;
+                console.log(score);
+            }
+        }
+        //console.log(score1);
+    }
+    display() {
+        
+        var pos = this.body.position
         var angle = this.body.angle;
 
         push();
